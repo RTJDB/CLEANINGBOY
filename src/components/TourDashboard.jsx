4d9b6391f5136eb570b256856c026a1082dd67c5
RTJDB <JAMES@RORSCHACHTEST.COM>
2025-04-01 22:58:26 -0700

    Initial commit

---
+++ src/components/TourDashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TourDashboard() {
+  return (
+    <div style={{ color: 'white', backgroundColor: 'black', minHeight: '100vh', padding: '2rem' }}>
+      <h1 style={{ color: 'red' }}>Testheads United</h1>
+      <p>Welcome to the Scars and Stripes Forever Tour Dashboard.</p>
+    </div>
+  );
+}
